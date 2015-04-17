df983fd1f2f5e9327f31c1e49e311cfd05ad061d
Mark Lee <mark.lee@capricasoftware.co.uk>
2015-04-17 09:26:13 +0100

    Describe how profile relates to account.

--- src/main/java/uk/co/caprica/bootlace/controller/profile/ProfileController.java
+++ src/main/java/uk/co/caprica/bootlace/controller/profile/ProfileController.java
@@ -41,6 +41,23 @@ import uk.co.caprica.bootlace.security.domain.UserWithId;
  * <p>
  * A profile is basic personal data associated with an application user.
  * <p>
+ * Semantically a user profile (for a valid user) always "exists" even if it has not been created
+ * in the repository yet.
+ * <p>
+ * This is <em>not</em> typical.
+ * <p>
+ * The security constraints are such that a profile will be returned only for the currently
+ * authenticated user, i.e. this service does not allow for a particular profile to be retrieved
+ * even if its unique identifier is known.
+ * <p>
+ * This is <em>not</em> typical either.
+ * <p>
+ * The profile information could just be kept with the user account information, but they are kept
+ * separate so as not to blur responsibilities - i.e. the user account data is deemed "system"
+ * data, whereas the profile is more like "application" data.
+ * <p>
+ * To simplify this a little bit, the profile used the same unique identifier as the user account.
+ * <p>
  * {@link RestController} adds {@link Controller} and {@link ResponseBody} by default.
  */
 @RestController
@@ -60,17 +77,6 @@ public class ProfileController {
 
     /**
      * Get the currently authenticated user profile resource.
-     * <p>
-     * Semantically a user profile (for a valid user) always "exists" even if it has not been
-     * created in the repository yet.
-     * <p>
-     * This is <em>not</em> typical.
-     * <p>
-     * The security constraints are such that a profile will be returned only for the currently
-     * authenticated user, i.e. this service does not allow for a particular profile to be
-     * retrieved even if its unique identifier is known.
-     * <p>
-     * This is <em>not</em> typical either.
      *
      * @param user currently authenticated user
      * @return profile
